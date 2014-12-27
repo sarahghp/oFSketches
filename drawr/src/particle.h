1f6004b4a9d76019dadef9e09ea41b142fbb3056
Sarah GP <sarah.groff.palermo@gmail.com>
2014-12-26 21:21:41 -0800

    dotDraw prepped for download

---
+++ drawr/src/particle.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "ofMain.h"
+
+class particle
+{
+    public:
+        ofVec2f pos;
+        ofVec2f vel;
+        ofVec2f frc;   // frc is also know as acceleration (newton says "f=ma")
+			
+        particle();
+		virtual ~particle(){};
+
+        void resetForce();
+        void addForce(float x, float y);
+        void addForce(const ofVec2f& frc);
+        void addDampingForce();
+        void setInitialCondition(float px, float py, float vx, float vy);
+        void update();
+        void draw();
+	
+		void bounceOffWalls();
+	
+	
+		float damping;
+
+    protected:
+    private:
+};
+
